d89917a0ad55ced2d039cafedcbf8223aadc0f58
Bogdan Matmusaev <bogdanmatmusave@gmail.com>
2026-04-06 01:17:09 +0500

    service-section adaptive

--- src/components/ServiceBlock.jsx
+++ src/components/ServiceBlock.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import ListElement from "./ListElement"
 
 const ServiceBlock = (props) => {
@@ -6,10 +7,36 @@ const ServiceBlock = (props) => {
         service,
     } = props
 
+	const [line, setLine] = useState(true)
+
+	const onClickLine = () => {
+		setLine(prev => !prev)
+	}
+
+	const secondClassName = line ? className : 'def-div'
+
     return (
-        <div className={className}>
-            <p className="service-block__paragraph">{service.paragraph}</p>
-            {service.svg}
+        <div className={secondClassName} >
+			<p className="service-block__paragraph">{service.paragraph}</p>
+			{service.svg}
+			<svg className="service-spread" width="68" height="68" viewBox="0 0 68 68" fill="none" xmlns="http://www.w3.org/2000/svg" onClick={onClickLine}>
+				<circle cx="34" cy="34" r="33" stroke="#DFB46B" stroke-width="2" />
+				<rect 
+					x="20" y="33.5" 
+					width="28" height="1" 
+					rx="1" 
+					fill="#424242" 
+					transform="rotate(-90 34 34)"
+					className={line ? '' : 'line-off'}
+				/>
+				<rect 
+					x="20" y="33.5" 
+					width="28" height="1" 
+					rx="1" 
+					fill="#424242" 
+					transform="rotate(-180 34 34)" 
+				/>
+			</svg>
             <ul className="service-block__list">
                 {service.list.map((string, index) => (
                     <ListElement
