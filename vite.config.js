c37fb0a1495133e9a9209e3e5f2bd5d9aa77335f
Bogdan Matmusaev <bogdanmatmusave@gmail.com>
2026-04-19 23:24:26 +0500

    доработки 2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/political-project/',
+  base: '/vite-political-site/',
 })
